29fe5d865e6bc99796d1e67908a352528eba841a
rossanodan <dangelo.rossano.94@gmail.com>
2019-12-26 20:55:10 +0000

    work in progress on header bar for navigation

--- src/App.jsx
+++ src/App.jsx
@@ -9,20 +9,32 @@ import styles from './App.module.scss';
 
 const App = () => {
   return (
-    <Switch>
-      <Route exact path='/'>
-        <Discover />
-      </Route>
-      <Route exact path='/discover/movie'>
-        <Discover />
-      </Route>
-      <Route exact path='/movie/:movie_id'>
-        <Movie />
-      </Route>
-      <Route>
-        <NotFound />
-      </Route>
-    </Switch>
+    <div>
+      <nav className={styles.header}>
+        <ul>
+          <li>
+            <a href="/">Discover</a>
+          </li>
+          <li>Genres</li>
+          <li>Genres</li>
+          <li>Trending</li>
+        </ul>
+      </nav>
+      <Switch>
+        <Route exact path='/'>
+          <Discover />
+        </Route>
+        <Route exact path='/discover/movie'>
+          <Discover />
+        </Route>
+        <Route exact path='/movie/:movie_id'>
+          <Movie />
+        </Route>
+        <Route>
+          <NotFound />
+        </Route>
+      </Switch>
+    </div>
   );
 }
 
